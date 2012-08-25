1f1d05121ae8b09528da9ea12c0827ec89aceba3
Anupam Sengupta <anupamsg@gmail.com>
2012-08-24 23:06:47 -0400

    Added Emacs settings for highlighting todo tags.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,8 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((nil . ((projectile-mode . t)
-         (fill-column . 80))))
+((nil
+  (projectile-mode . t)
+  (fill-column . 80))
+ (ruby-mode .
+  ((eval . (highlight-regexp "@todo")))))
