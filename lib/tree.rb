16147614f9ea7776658b1386b77f5823cdb68a51
Anupam Sengupta <anupamsg@gmail.com>
2012-08-25 22:57:41 -0400

    Factored out the JSON conversion code.

--- lib/tree.rb
+++ lib/tree.rb
@@ -42,6 +42,7 @@
 require 'tree/tree_deps'
 require 'tree/version'
 require 'tree/utils/camel_case_method_handler'
+require 'tree/utils/json_converter'
 
 # This module provides a TreeNode class which is the primary class for representing
 # nodes in the tree.
@@ -132,6 +133,7 @@ module Tree
   class TreeNode
     include Enumerable
     include Tree::Utils::CamelCaseMethodHandler
+    include Tree::Utils::JSONConverter
 
     # @!attribute [r] name
     #
@@ -833,77 +835,6 @@ module Tree
           nodes[name] = self    # Add self to the list of nodes
         end
       end
-    end
-
-    # Creates a JSON ready Hash for the #to_json method.
-    #
-    # @author Eric Cline (https://github.com/escline)
-    # @since 0.8.3
-    #
-    # @return A hash based representation of the JSON
-    #
-    # Rails uses JSON in ActiveSupport, and all Rails JSON encoding goes through as_json
-    #
-    # @see Tree::TreeNode.to_json
-    # @see http://stackoverflow.com/a/6880638/273808
-    def as_json(options = {})
-
-        json_hash = {
-          "name"         => name,
-          "content"      => content,
-          JSON.create_id => self.class.name
-        }
-
-        if has_children?
-          json_hash["children"] = children
-        end
-
-        return json_hash
-
-    end
-
-    # Creates a JSON representation of this node including all it's children.   This requires the JSON gem to be
-    # available, or else the operation fails with a warning message.
-    # Uses the Hash output of as_json method, defined above.
-    #
-    # @author Dirk Breuer (http://github.com/railsbros-dirk)
-    # @since 0.7.0
-    #
-    # @return The JSON representation of this subtree.
-    #
-    # @see Tree::TreeNode.json_create
-    # @see Tree::TreeNode.as_json
-    # @see http://flori.github.com/json
-    def to_json(*a)
-      as_json.to_json(*a)
-    end
-
-    # Helper method to create a Tree::TreeNode instance from the JSON hash representation.  Note that this method should
-    # *NOT* be called directly.  Instead, to convert the JSON hash back to a tree, do:
-    #
-    # tree = JSON.parse (the_json_hash)
-    #
-    # This operation requires the JSON gem to be available, or else the operation fails with a warning message.
-    #
-    # @author Dirk Breuer (http://github.com/railsbros-dirk)
-    # @since 0.7.0
-    #
-    # @param [Hash] json_hash The JSON hash to convert from.
-    #
-    # @return [Tree::TreeNode] The created tree.
-    #
-    # @see #to_json
-    # @see http://flori.github.com/json
-    def self.json_create(json_hash)
-
-      node = new(json_hash["name"], json_hash["content"])
-
-      json_hash["children"].each do |child|
-        node << child
-      end if json_hash["children"]
-
-      return node
-
     end
 
     # @!attribute [r] node_height
