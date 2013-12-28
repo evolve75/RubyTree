3483d04526c7f9c6b42d013f1594ad527b03638d
Anupam Sengupta <anupamsg@gmail.com>
2013-12-28 11:17:54 -0500

    Moved the tree metrics methods to a new mixin.

--- lib/tree.rb
+++ lib/tree.rb
@@ -41,6 +41,7 @@
 
 require 'tree/tree_deps'
 require 'tree/version'
+require 'tree/utils/metrics_methods'
 require 'tree/utils/camel_case_method_handler'
 require 'tree/utils/json_converter'
 
@@ -132,6 +133,7 @@ module Tree
   # @author Anupam Sengupta
   class TreeNode
     include Enumerable
+    include Tree::Utils::TreeMetricsHandler
     include Tree::Utils::CamelCaseMethodHandler
     include Tree::Utils::JSONConverter
 
@@ -614,28 +616,6 @@ module Tree
 
     # @!endgroup
 
-    # @!attribute [r] size
-    # Total number of nodes in this (sub)tree, including the receiver node.
-    #
-    # Size of the tree is defined as:
-    #
-    # Size:: Total number nodes in the subtree including the receiver node.
-    #
-    # @return [Integer] Total number of nodes in this (sub)tree.
-    def size
-      inject(0) {|sum, node| sum + 1 }
-    end
-
-    # Convenience synonym for {Tree::TreeNode#size}.
-    #
-    # @deprecated This method name is ambiguous and may be removed.  Use TreeNode#size instead.
-    #
-    # @return [Integer] The total number of nodes in this (sub)tree.
-    # @see #size
-    def length
-      size()
-    end
-
     # Pretty prints the (sub)tree rooted at the receiver node.
     #
     # @param [Integer] level The indentation level (4 spaces) to start with.
@@ -870,97 +850,6 @@ module Tree
           nodes[name] = self    # Add self to the list of nodes
         end
       end
-    end
-
-    # @!attribute [r] node_height
-    # Height of the (sub)tree from the receiver node.  Height of a node is defined as:
-    #
-    # Height:: Length of the longest downward path to a leaf from the node.
-    #
-    # - Height from a root node is height of the entire tree.
-    # - The height of a leaf node is zero.
-    #
-    # @return [Integer] Height of the node.
-    def node_height
-      return 0 if is_leaf?
-      1 + @children.collect { |child| child.node_height }.max
-    end
-
-    # @!attribute [r] node_depth
-    # Depth of the receiver node in its tree.  Depth of a node is defined as:
-    #
-    # Depth:: Length of the node's path to its root.  Depth of a root node is zero.
-    #
-    # *Note* that the deprecated method Tree::TreeNode#depth was incorrectly computing this value.
-    # Please replace all calls to the old method with Tree::TreeNode#node_depth instead.
-    #
-    # 'level' is an alias for this method.
-    #
-    # @return [Integer] Depth of this node.
-    def node_depth
-      return 0 if is_root?
-      1 + parent.node_depth
-    end
-
-    alias level node_depth       # Aliased level() method to the node_depth().
-
-    # Returns depth of the tree from the receiver node. A single leaf node has a depth of 1.
-    #
-    # This method is *DEPRECATED* and may be removed in the subsequent releases.
-    # Note that the value returned by this method is actually the:
-    #
-    # _height_ + 1 of the node, *NOT* the _depth_.
-    #
-    # For correct and conventional behavior, please use {Tree::TreeNode#node_depth} and
-    # {Tree::TreeNode#node_height} methods instead.
-    #
-    # @return [Integer] depth of the node.
-    # @deprecated This method returns an incorrect value.  Use the 'node_depth' method instead.
-    #
-    # @see #node_depth
-    def depth
-      warn DeprecatedMethodWarning, 'This method is deprecated.  Please use node_depth() or node_height() instead (bug # 22535)'
-
-      return 1 if is_leaf?
-      1 + @children.collect { |child| child.depth }.max
-    end
-
-    # @!attribute [r] breadth
-    # Breadth of the tree at the receiver node's level.
-    # A single node without siblings has a breadth of 1.
-    #
-    # Breadth is defined to be:
-    # Breadth:: Number of sibling nodes to this node + 1 (this node itself),
-    # i.e., the number of children the parent of this node has.
-    #
-    # @return [Integer] breadth of the node's level.
-    def breadth
-      is_root? ? 1 : parent.children.size
-    end
-
-    # @!attribute [r] in_degree
-    # The incoming edge-count of the receiver node.
-    #
-    # In-degree is defined as:
-    # In-degree:: Number of edges arriving at the node (0 for root, 1 for all other nodes)
-    #
-    # - In-degree = 0 for a root or orphaned node
-    # - In-degree = 1 for a node which has a parent
-    #
-    # @return [Integer] The in-degree of this node.
-    def in_degree
-      is_root? ? 0 : 1
-    end
-
-    # @!attribute [r] out_degree
-    # The outgoing edge-count of the receiver node.
-    #
-    # Out-degree is defined as:
-    # Out-degree:: Number of edges leaving the node (zero for leafs)
-    #
-    # @return [Integer] The out-degree of this node.
-    def out_degree
-      is_leaf? ? 0 : children.size
     end
 
     protected :parent=, :set_as_root!, :create_dump_rep
