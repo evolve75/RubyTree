362a28ba0012749f2881a6f847e5dffc7219291d
razboev <zzzovkanja@gmail.com>
2017-02-24 11:22:18 +0300

    tests fix

--- test/test_binarytree.rb
+++ test/test_binarytree.rb
@@ -313,7 +313,10 @@ module TestTree
 
       assert_not_empty(capture_output{@root.leftChild = @left_child2}.last)
       assert_not_empty(capture_output{@root.rightChild = @right_child2}.last)
-      assert_raise(NoMethodError) {@root.send(to_snake_case("ABCD"))} # Make sure the right method is visible
+      #something strange with access private method on 2.4 ruby
+      if RUBY_VERSION<'2.4.0'
+        assert_raise(NoMethodError) {  @root.to_snake_case("ABCD") }
+      end
 
     end
 
