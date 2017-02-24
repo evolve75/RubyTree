fe8be2388b90c15235cafe6841275906f4038fcb
razboev <zzzovkanja@gmail.com>
2017-02-24 18:10:31 +0300

    self included private method tests fixed to differenr ruby versions

--- test/test_binarytree.rb
+++ test/test_binarytree.rb
@@ -313,8 +313,8 @@ module TestTree
 
       assert_not_empty(capture_output{@root.leftChild = @left_child2}.last)
       assert_not_empty(capture_output{@root.rightChild = @right_child2}.last)
-      #something strange with access private method on 2.4 ruby
-      if RUBY_VERSION<'2.4.0'
+      #ruby self include private policy changed in 2.3
+      if RUBY_VERSION<'2.3.0'
         assert_raise(NoMethodError) {  @root.to_snake_case("ABCD") }
       end
 
